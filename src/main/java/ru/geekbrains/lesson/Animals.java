7ce00237b984c0c26ac26cde63415e2a92188836
Bersserk <bers.developer@gmail.com>
2020-11-06 01:44:14 +0100

    Task 5 Done

--- src/main/java/ru/geekbrains/lesson/Animals.java
+++ src/main/java/ru/geekbrains/lesson/Animals.java
@@ -1,4 +1,67 @@
 package ru.geekbrains.lesson;
 
 public class Animals {
+
+    private String name;
+
+    private float maxRun;
+    private float maxJump;
+    private float maxSwim;
+
+    public Animals(String name, float limitRun, float limitJump, float limitSwim) {
+        this.name = name;
+        this.maxRun = limitRun;
+        this.maxJump = limitJump;
+        this.maxSwim = limitSwim;
+    }
+
+    public Animals(String name, float limitRun, float limitJump) {
+        this.name = name;
+        this.maxRun = limitRun;
+        this.maxJump = limitJump;
+    }
+
+
+    protected void run (float run){
+        ifAndAction("run", run, maxRun);
+    }
+
+    protected void jump (float jump){
+        ifAndAction("jump", jump, maxJump);
+    }
+
+    protected void swim (float swim){
+        ifAndAction("swim", swim, maxSwim);
+    }
+
+    //   сравнивает действие с допустимым и выдает результат
+    private void ifAndAction (String act, float action, float maxAction){
+
+        if (action > maxAction) {
+            switch (act){
+                case "swim":
+                    System.out.println("На такое расстояние " + name + " плавать не могёт!");
+                    break;
+                case "jump":
+                    System.out.println("На такую высоту " + name + " прыгнуть не могёт!");
+                    break;
+                case "run":
+                    System.out.println("Столько " + name + " пробежать не могёт !");
+                    break;
+            }
+
+        } else {
+            switch (act){
+                case "swim":
+                    System.out.println(name + " проплыл!!!");
+                    break;
+                case "jump":
+                    System.out.println(name + " прыгнул!!!");
+                    break;
+                case "run":
+                    System.out.println(name + " пробежал !");
+                    break;
+            }
+        }
+    }
 }
--- src/main/java/ru/geekbrains/lesson/Main.java
+++ src/main/java/ru/geekbrains/lesson/Main.java
@@ -4,10 +4,69 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Dog sharik = new Dog("Шарик", 500, 0.5f, 50);
+        sharik.run(55);
+        sharik.jump(30);
+        sharik.swim(55);
+
+        Dog tuzik = new Dog("Тузик", 300, 0.5f, 50);
+        tuzik.run(55);
+        tuzik.jump(0.7f);
+        tuzik.swim(45);
+
+        Cat murzik = new Cat("Мурзик", 200, 2);
+        murzik.swim(20);
+        murzik.run(30);
+        murzik.jump(45);
+
+        Cat vaska = new Cat("Васька", 20, 15);
+        vaska.swim(20);
+        vaska.run(30);
+        vaska.jump(45);
+
+        Horse poni = new Horse("Пони", 1500, 3, 100);
+        poni.swim(80);
+        poni.run(1200);
+        poni.jump(10);
+
+        Bird kesha = new Bird("Кеша", 5, 0.2f);
+        kesha.swim(20);
+        kesha.jump(0.5f);
+        kesha.run(3);
+    }
+}
+
+class Dog extends Animals {
+
+    public Dog(String setName, float setLimitRun, float setLimitJump, float setLimitSwim) {
+        super(setName, setLimitRun, setLimitJump, setLimitSwim);
+    }
+}
+
+class Horse extends Animals {
+    public Horse(String setName, float setLimitRun, float setLimitJump, float setLimitSwim) {
+        super(setName, setLimitRun, setLimitJump, setLimitSwim);
+    }
+}
+
+class Bird extends Animals {
+    public Bird(String setName, float setLimitRun, float setLimitJump) {
+            super(setName, setLimitRun, setLimitJump);
+    }
+
+    protected void swim(float swim) {
+        System.out.println("Птицы плавать не умеют");
+    }
+}
+
+class Cat extends Animals {
+    public Cat(String setName, float setLimitRun, float setLimitJump) {
+        super(setName, setLimitRun, setLimitJump);
+    }
+
+    protected void swim(float swim) {
+        System.out.println("Коты плавать не умеют");
     }
 }
 
 
-class Dog extends Animals {
-
-}
