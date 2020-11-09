f56b61aab9edf83074fa7366a541c72d965f9ae5
Bersserk <bers.developer@gmail.com>
2020-11-09 23:14:41 +0100

    Create template

---
+++ src/main/java/ru/geekbrains/lesson/Game.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lesson;
+
+public class Game {
+    public static void main(String[] args) {
+        new MainWindow();
+        System.out.println("method main is end");
+    }
+}
+
