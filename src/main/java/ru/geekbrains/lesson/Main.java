9cc6b2c4e10f9ce3413cce3a53d3526b6914a80a
Bersserk <bers.developer@gmail.com>
2020-12-01 19:17:36 +0100

    got the number of character in the string

--- src/main/java/ru/geekbrains/lesson/Main.java
+++ src/main/java/ru/geekbrains/lesson/Main.java
@@ -2,7 +2,57 @@ package ru.geekbrains.lesson;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static String string = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
+    public static String [][] arrStr;
+    static int i = 0;
+    static int j = 0;
+    static String tmp;
 
+    public static void main(String[] args) {
+        arrStr = new String[4][4];
+
+        tmp = string.replaceAll("\\r|\\n", " ");
+        String [] tmp2 = tmp.split(" ");
+
+        //String [] str = string.split();
+
+        //String s = str.toString();
+
+        System.out.println(string.replaceAll("\\r|\\n", " ").split(" ").length);
+
+
+
+        //System.out.println(tmp);
+
+//        for (String s: str){
+//            if(j == 4){
+//                i++;
+//                j=0;
+//            }
+//            arrStr[i][j] = s;
+//            j++;
+//        }
+
+
+//
+//        int k = 0;
+//
+//        for (int i = 0; i < arrStr.length; i++) {
+//            for (int j = 0; j < arrStr.length; j++) {
+//                if (string.substring(k) != " ")
+//                arrStr[i][j] = string.substring(k);
+//                k++;
+//            }
+//        }
+
+
+
+    }
+
+    public static int [][] matrixToArray (String matrix){
+
+
+
+        return null;
     }
 }
